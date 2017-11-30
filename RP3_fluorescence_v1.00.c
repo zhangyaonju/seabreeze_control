6dcf0ed9e336cb119f7feb05af38e23167773f8c
Yao Zhang <zhangyaonju@gmail.com>
2017-11-29 20:43:40 -0600

    add fixed time operation for QE-pro 2s&1s
    2s for irradiance and 1s for radiance

--- RP3_fluorescence_v1.00.c
+++ RP3_fluorescence_v1.00.c
@@ -7,13 +7,17 @@ For every 5 minutes, the crontab call this program to complete the following pro
 		a. will output the enclosure temperature first
 		b. start the cooling fuction and record the temepurature before it is stable
 	3. get the optimum integration time for both HR2000+ and QE-pro
-	4. start 20 pairs of observation (both for irradiance and reflectance)
+	4. start 10 pairs of observation (both for irradiance and reflectance)
 	5. do the dark correction and nonlinearity correction
 	6. get the time info and output the spectrum to the destination folder (/home/pi/spectrum_data/).
 	
    20170515 updates: 
    1. change TEC temperature control, heat up to 20 and then cool down to -10 degree C
    2. change allowed DN range for optimal integration time to 100000 ~ 140000 for QE-pro and 9000~12000 for HR-2000+
+   20171129 updates:
+   1. add two additional measurement with fixed integration time (Only for QE-pro 2000 ms for irradiance and 1000 ms for radiance)
+   	additional dark measurements for the irradiance and radiance measurements
+	will be saved in a separate file
 */
 
 #include <stdio.h>
@@ -357,6 +361,7 @@ int main(){
 			HR2000PLUS_operation(device_ids[i], maxCount);
 		}else if(strcmp(nameBuffer,"QE-PRO")==0){
 			QE_PRO_operation(device_ids[i], maxCount);
+			QE_PRO_fixed_fime_operation(device_ids[i], maxCount);
 		}else{
 			printf("\n\nUnsupported Spectrometer: %s \n\n", nameBuffer);
 			free(device_ids);
@@ -760,6 +765,261 @@ void QE_PRO_operation(long deviceID, int Operation_ct){
 	
 	//get optimum integration time2
 	irrInteTime2 = get_opt_integration_time_QEpro(deviceID, spectrometer_ids[0]);
+	sbapi_spectrometer_set_integration_time_micros(deviceID, spectrometer_ids[0], &error, irrInteTime2);
+	printf("\n\nSet integration time to %ld ms [%s]\n", irrInteTime2/1000, sbapi_get_error_string(error));
+	
+	//print integration time to file
+	fprintf(f,"integration time 2: %ld\n", irrInteTime2);
+	
+	//turn on the internal shutter to get the dark measurement 
+	set_internal_shutter_for_QE_pro(deviceID, 1);
+	sleep(2);
+	spectrum_dark2 = (double *)calloc((size_t)spec_length, sizeof(double));
+	sbapi_data_buffer_clear(deviceID, buffer_ids[0], &error);
+	flag = sbapi_spectrometer_get_formatted_spectrum(deviceID, spectrometer_ids[0], &error, spectrum_dark2, spec_length);
+	printf("\nGetting a dark measurement spectrum.\n");
+	//turn off the internal shutter to get next measurement
+	set_internal_shutter_for_QE_pro(deviceID, 0);
+	for (int m = 0; m < spec_length-1; m++){				
+		fprintf(f,"%lf,", spectrum_dark2[m]);
+	}
+	fprintf(f,"%f\n\n",spectrum_dark2[spec_length-1]);
+	
+	free(spectrum_dark1);
+	free(spectrum_dark2);
+	
+	if(spec_length > 0){
+		//dark correction coefficient
+		//dark pixels are [0~3, 1040~1043] for QE-pro
+		//start the spectrum collection based on the preset sampling numbers
+		for (int j = 0; j < Operation_ct; j++){
+			//turn off the lamp to get spectrum using irrInteTime1
+			sbapi_lamp_set_lamp_enable(deviceID,lamp_ids[0], &error, 1);
+			sbapi_spectrometer_set_integration_time_micros(deviceID, spectrometer_ids[0], &error, irrInteTime1);
+			sleep(2);
+			
+			spectrum1 = (double *)calloc((size_t)spec_length, sizeof(double));
+			printf("\nGetting a formatted spectrum.\n");
+			sbapi_data_buffer_clear(deviceID, buffer_ids[0], &error);			
+			flag = sbapi_spectrometer_get_formatted_spectrum(deviceID, spectrometer_ids[0], &error, spectrum1, spec_length);			
+			max_v = max_array(spectrum1, spec_length);
+
+			printf("...Result is (%d) [%s] max_val %lf integtation time %ld\n", flag, 
+				sbapi_get_error_string(error), max_v, irrInteTime1);
+			for (int m = 0; m < spec_length-1; m++){				
+				fprintf(f,"%lf,", spectrum1[m]);
+			}
+			fprintf(f,"%lf\n", spectrum1[spec_length-1]);
+
+			sleep(2);
+			///////////////////////////////////////////////////////////////////////////////////////
+			//turn on the lamp to get spectrum using irrInteTime2
+			sbapi_lamp_set_lamp_enable(deviceID,lamp_ids[0], &error, 0);
+			sbapi_spectrometer_set_integration_time_micros(deviceID, spectrometer_ids[0], &error, irrInteTime2);
+			sleep(2);
+			
+			spectrum2 = (double *)calloc((size_t)spec_length, sizeof(double));
+			printf("\nGetting a formatted spectrum.\n");
+			sbapi_data_buffer_clear(deviceID, buffer_ids[0], &error);			
+			flag = sbapi_spectrometer_get_formatted_spectrum(deviceID, spectrometer_ids[0], &error, spectrum2, spec_length);			
+			max_v = max_array(spectrum2, spec_length);
+
+			printf("...Result is (%d) [%s] max_val %lf integtation time %ld\n", flag, 
+				sbapi_get_error_string(error), max_v, irrInteTime2);
+			for (int m = 0; m < spec_length-1; m++){				 
+				fprintf(f,"%lf,", spectrum2[m]);
+			}
+			fprintf(f,"%lf\n", spectrum2[spec_length-1]);
+
+			free(spectrum1);
+			free(spectrum2);
+
+			sleep(2);
+		}
+	}
+	
+	
+	fclose(f);
+
+	printf("\n\nAttempting to close:\n");
+
+
+	sbapi_tec_set_enable(deviceID, tec_ids[0], &error, 0);
+	sbapi_close_device(deviceID, &error);
+	printf("\tResult is (%d) [%s]\n", flag, sbapi_get_error_string(error));
+}
+
+void QE_PRO_fixed_fime_operation(long deviceID, int Operation_ct){
+	int error;
+	int spec_length;
+	int number_of_spectrometers = 0;
+	int number_of_nonlinearity_coeff_features;
+	int flag;
+	int length_nonlinear_coeff;
+	int number_of_lamps;
+	int number_of_tec;
+	int number_of_buffer;
+	long *buffer_ids = 0;
+	long *lamp_ids = 0;
+	long *tec_ids = 0;
+	long irrInteTime1 = 2000000;  //for irradiance
+	long irrInteTime2 = 1000000;  //for radiance
+	long *spectrometer_ids;
+	long *nonlinearity_coeff_feature_ids = 0;
+	float temperature;
+	double *wavelengths;
+	double *spectrum1 = 0;
+	double *spectrum2 = 0;
+	double *spectrum_dark1 = 0;
+	double *spectrum_dark2 = 0;
+	double nonlinear_coeff[8];
+	double max_v;
+
+	char fname[100];
+	char *dateonly;		//date without day and replace space with "_"	
+
+	char *timestring;
+	time_t rawtime;
+	struct tm *timeinfo;
+	
+	/* Open the device */
+	printf("\tAttempting to open:\n");
+	flag = sbapi_open_device(deviceID, &error);
+	printf("\t\tResult is (%d) [%s]\n", flag, sbapi_get_error_string(error));
+	
+	if(flag != 0) {
+		return;
+	}
+	
+	//get system time for the output file name
+	rawtime = time(0);
+	timeinfo = localtime(&rawtime);
+	
+	timestring = asctime(timeinfo);
+	RemoveSpaces(timestring);
+	dateonly = &(timestring[4]);
+	printf("\nToday is %s\n",dateonly);
+	
+	number_of_spectrometers = sbapi_get_number_of_spectrometer_features(deviceID, &error);
+	spectrometer_ids = (long *)calloc(number_of_spectrometers, sizeof(long));
+	number_of_spectrometers = sbapi_get_spectrometer_features(deviceID, &error,
+		spectrometer_ids, number_of_spectrometers);
+	
+	sbapi_spectrometer_set_trigger_mode(deviceID,spectrometer_ids[0], &error, 0);
+	
+	//get tec temperature for QE-pro
+	number_of_tec = sbapi_get_number_of_thermo_electric_features(deviceID, &error);
+	tec_ids = (long *)calloc(number_of_tec,sizeof(long));
+	number_of_tec = sbapi_get_thermo_electric_features(deviceID, &error, tec_ids, number_of_tec);
+	
+	temperature = (float)sbapi_tec_read_temperature_degrees_C(deviceID, tec_ids[0],&error);
+	
+	//get the name of the output file;
+	fname[0] = '\0';
+ 	strcat(fname,"/home/pi/spectrum_data/");
+	strcat(fname,"fix_QE_PRO");
+	strcat(fname,"_");
+	strcat(fname,dateonly);
+	strcat(fname,".csv");
+	printf("\nOutput file name: %s\n",fname);
+	
+	//open file and print date and time
+	FILE *f = fopen(fname, "wb");
+	fprintf(f,"observation date and time: %s\n", asctime(timeinfo));
+	//print spectrometers information to file;
+	fprintf(f,"spectrometer: QE-PRO\n");
+	//print system temperature to file:
+	fprintf(f,"ambient temperature: %1.2f\n",temperature);
+	
+	
+	//enable the TEC and set the temperature to -10 degree
+	sbapi_tec_set_enable(deviceID, tec_ids[0], &error, 1);
+	sbapi_tec_set_temperature_setpoint_degrees_C(deviceID,tec_ids[0],&error, -10.0);
+	
+	//wait for 5 second for the TEC to work and reach the optimum temperature
+	sleep(5);
+	temperature = (float)sbapi_tec_read_temperature_degrees_C(deviceID, tec_ids[0],&error);
+	//print system temperature to file:
+	fprintf(f,"measuring temperature: %1.2f\n",temperature);
+
+		
+	//get the nonlinearity correction coefficients
+	number_of_nonlinearity_coeff_features = sbapi_get_number_of_nonlinearity_coeffs_features(deviceID, &error);
+	nonlinearity_coeff_feature_ids = (long *)calloc(number_of_nonlinearity_coeff_features, sizeof(long));
+	number_of_nonlinearity_coeff_features = sbapi_get_nonlinearity_coeffs_features(
+		deviceID, &error, nonlinearity_coeff_feature_ids, number_of_nonlinearity_coeff_features);
+	length_nonlinear_coeff = sbapi_nonlinearity_coeffs_get(deviceID, nonlinearity_coeff_feature_ids[0], &error, 
+		nonlinear_coeff, 8);
+	fprintf(f, "nonlinearity correction coefficients:\n");
+	for (int c = 0; c < (length_nonlinear_coeff - 1); c++){
+		fprintf(f, "%e,", nonlinear_coeff[c]);
+	}
+	fprintf(f, "%e\n", nonlinear_coeff[length_nonlinear_coeff - 1]);
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	spec_length = sbapi_spectrometer_get_formatted_spectrum_length(deviceID, spectrometer_ids[0],&error);
+	wavelengths = (double *)calloc((size_t)spec_length, sizeof(double));
+	flag = sbapi_spectrometer_get_wavelengths(deviceID,
+		spectrometer_ids[0], &error, wavelengths, spec_length);
+	//print wavelengths to file
+	for (int m = 0; m < spec_length-1; m++){
+		fprintf(f,"%f,", wavelengths[m]);
+	}
+	fprintf(f,"%f\n",wavelengths[spec_length-1]);
+	free(wavelengths);
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	printf("\t\t\tAttempting to set trigger mode to 0\n");
+        sbapi_spectrometer_set_trigger_mode(deviceID,
+                spectrometer_ids[0], &error, 0);
+
+	//collect signal for irradiance
+	//set the lamp off to get irradiance
+	number_of_lamps = sbapi_get_number_of_lamp_features(deviceID, &error);
+	lamp_ids = (long *)calloc(number_of_lamps, sizeof(long));
+	number_of_lamps = sbapi_get_lamp_features(deviceID, &error,
+		lamp_ids, number_of_lamps);
+	sbapi_lamp_set_lamp_enable(deviceID,
+		lamp_ids[0], &error, 1);
+	printf("\n\t\t\tTurn the lamp on to get irradiance [%s]\n",sbapi_get_error_string(error));
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	number_of_buffer = sbapi_get_number_of_data_buffer_features(deviceID, &error);
+	buffer_ids = (long *)calloc(number_of_buffer,sizeof(long));
+	number_of_buffer = sbapi_get_data_buffer_features(deviceID, &error,
+		buffer_ids, number_of_buffer);
+	//sbapi_data_buffer_clear(deviceID, buffer_ids[0], &error);
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	sleep(2);
+	//use fixed integration time1 = 2000 ms
+	sbapi_spectrometer_set_integration_time_micros(deviceID, spectrometer_ids[0], &error, irrInteTime1);
+	printf("\n\nSet integration time to %ld ms [%s]\n", irrInteTime1/1000, sbapi_get_error_string(error));
+
+	//print integration time to file
+	fprintf(f,"integration time 1: %ld\n", irrInteTime1);
+	
+	//turn on the internal shutter to get the dark measurement 
+	set_internal_shutter_for_QE_pro(deviceID, 1);
+	sleep(2);
+	spectrum_dark1 = (double *)calloc((size_t)spec_length, sizeof(double));
+	sbapi_data_buffer_clear(deviceID, buffer_ids[0], &error);
+	flag = sbapi_spectrometer_get_formatted_spectrum(deviceID, spectrometer_ids[0], &error, spectrum_dark1, spec_length);
+	printf("\nGetting a dark measurement spectrum.\n");
+	//turn off the internal shutter to get next measurement
+	set_internal_shutter_for_QE_pro(deviceID, 0);
+	for (int m = 0; m < spec_length-1; m++){				
+		fprintf(f,"%lf,", spectrum_dark1[m]);
+	}
+	fprintf(f,"%f\n",spectrum_dark1[spec_length-1]);
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	//set the lamp on for another radiance measurement
+	sbapi_lamp_set_lamp_enable(deviceID,
+		lamp_ids[0], &error, 0);
+	printf("\n\t\t\tTurn the lamp off to get irradiance %s\n",sbapi_get_error_string(error));
+	sleep(2);
+	
+	//use fixed integration time2 =1000 ms
 	sbapi_spectrometer_set_integration_time_micros(deviceID, spectrometer_ids[0], &error, irrInteTime2);
 	printf("\n\nSet integration time to %ld ms [%s]\n", irrInteTime2/1000, sbapi_get_error_string(error));
 	
